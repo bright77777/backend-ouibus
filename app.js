47e6b2f23201ce00fb667b0f077ec001dbcae794
bright77777 <bright77777@gmail.com>
2024-11-23 22:49:06 +0100

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bonjour bright , votre back est pret!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
